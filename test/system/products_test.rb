ed8f0d4ba27e62a650ef8c8589d1d5334763f811
Federico Capaldo <federico.capaldo1@gmail.com>
2022-09-08 12:14:37 +0100

    added product fixtures with faker, added users fixture, added system test with TDD to create new product and completed the new product functionality

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -4,5 +4,22 @@ class ProductTest < ApplicationSystemTestCase
   test "visit index" do
     visit root_url
     assert_selector "h1", text: "Awesome Products"
+    assert_selector ".card-product", count: Product.count
+  end
+
+
+  test "signed in user can create a new product" do
+    login_as users(:george)
+    visit "products/new"
+    save_screenshot
+
+    fill_in "product_name", with: "Le Wagon"
+    fill_in "product_tagline", with: "Change your life: Learn to code"
+    save_screenshot
+
+    click_on "Create Product"
+    save_screenshot
+    
+    assert_text "Change your life: Learn to code"
   end
 end
